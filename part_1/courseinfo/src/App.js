170de67eb5827c2eaaabc4c1baa8b08035635a2d
epixieme <kirstie_hayes@hotmail.com>
2023-06-07 10:43:15 +0100

    exercise 1.5

--- part_1/courseinfo/src/App.js
+++ part_1/courseinfo/src/App.js
@@ -1,24 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   const Header = (props) => {
+    // console.log(props.course.name)
     return (
       <header>
-        <h1>{props.course}</h1>
+        <h1>{props.course.name}</h1>
       </header>
     );
   };
@@ -42,9 +45,11 @@ const App = () => {
   };
 
   const Total = (props) => {
-  
-    const parts = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
- 
+    const parts =
+      props.parts[0].exercises +
+      props.parts[1].exercises +
+      props.parts[2].exercises;
+
     return (
       <section>
         <p>Number of exercises {parts} </p>
@@ -54,9 +59,9 @@ const App = () => {
 
   return (
     <div>
-       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
