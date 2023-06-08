d04d703f68f267ad2b1c394158b3ee5628527453
epixieme <kirstie_hayes@hotmail.com>
2023-06-08 12:39:05 +0100

    exercise 1.6

--- part_1/unicafe/src/App.js
+++ part_1/unicafe/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  
+  const Headers = ({ text }) => <h1>{text}</h1>;
+
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const Button = ({ text, onClick }) => {
+    return <button onClick={onClick}>{text}</button>;
+  };
+
+  const handleGoodClick = () => {
+    console.log("clicked");
+
+    const newClicks = {
+      ...clicks,
+      good: clicks.good + 1,
+    };
+    setClicks(newClicks);
+  };
+
+  const handleBadClick = () => {
+    console.log("clicked");
+
+    const newClicks = {
+      ...clicks,
+      bad: clicks.bad + 1,
+    };
+    setClicks(newClicks);
+  };
+  const handleNeutralClick = () => {
+    console.log("clicked");
+
+    const newClicks = {
+      ...clicks,
+      neutral: clicks.neutral + 1,
+    };
+    setClicks(newClicks);
+  };
+
+  const Total = ({ clicks, text }) => (
+    <section>
+      <p>
+        {text} {clicks}
+      </p>
+    </section>
   );
-}
+
+  return (
+    <section>
+      <Headers text='Give Feedback'/>
+      <Button text="Good" onClick={handleGoodClick} />
+      <Button text="Bad" onClick={handleBadClick} />
+      <Button text="Neutral" onClick={handleNeutralClick} />
+      <Headers text='Statistics'/>
+      <Total clicks={clicks.good} text="good" />
+      <Total clicks={clicks.bad} text="bad" />
+      <Total clicks={clicks.neutral} text="neutral" />
+    </section>
+  );
+};
 
 export default App;
