79153ad5ae247499fea85c0477e1a44255086be0
epixieme <kirstie_hayes@hotmail.com>
2023-06-12 11:14:51 +0100

    exercise 2.2 -2.3

--- part_2/courseinfo/src/components/Course.js
+++ part_2/courseinfo/src/components/Course.js
@@ -1,8 +1,8 @@
 const Course = ({ course }) => {
   return (
     <section>
-      <Header text={course.name} key = {course.id} />
-      <Content parts={course.parts}/>
+      <Header text={course.name} key={course.id} />
+      <Content parts={course.parts} />
     </section>
   );
 };
@@ -16,22 +16,37 @@ const Header = ({ text }) => {
 };
 
 const Content = ({ parts }) => {
+const sum = parts.map(item=>item.exercises).reduce((a,c)=>a+c,0)
   return (
     <section>
       {parts.map((item) => (
-       
-          <Part part={item.name} exercises= {item.exercises} key={item.id}/>
-     
+        <Part part={item.name} exercises={item.exercises} key={item.id} />
       ))}
+      <Stats sum={sum} />
     </section>
   );
 };
 
-const Part = ({ part, exercises}) => {
+const Part = ({ part, exercises }) => {
   return (
     <section>
-      <p>{part} {exercises}</p>
+      <p>
+        {part} {exercises}
+      </p>
     </section>
   );
 };
+
+const Stats = ({sum}) =>{
+
+    return (
+        <section>
+          <h4>
+            Total of {sum} exercises
+          </h4>
+        </section>
+      );
+
+}
+
 export default Course;
