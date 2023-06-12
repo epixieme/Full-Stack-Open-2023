74f4c05f2ba7a57e84fcc9ec56245f01a8a2a15e
epixieme <kirstie_hayes@hotmail.com>
2023-06-12 13:35:56 +0100

    exercise 2.4-2.5

--- part_2/courseinfo/src/components/Course.js
+++ part_2/courseinfo/src/components/Course.js
@@ -1,48 +1,38 @@
+const Part = ({ part, exercises }) => (
+  <section>
+    <p>
+      {part} {exercises}
+    </p>
+  </section>
+);
+
+const Stats = ({ sum }) => {
+  const total = sum.reduce((a, c) => a + c.exercises, 0);
+  return (
+    <section>
+      <h4>Total of {total} exercises</h4>
+    </section>
+  );
+};
+
+const Content = ({ parts }) =>
+  parts.map((item) => (
+    <Part part={item.name} exercises={item.exercises} key={item.id} />
+  ));
+
+const Header = ({ text }) => (
+  <section>
+    <h1>{text}</h1>{" "}
+  </section>
+);
+
 const Course = ({ course }) => {
+  console.log(course);
   return (
     <section>
       <Header text={course.name} key={course.id} />
       <Content parts={course.parts} />
       <Stats sum={course.parts} />
-    </section>
-  );
-};
-
-const Header = ({ text }) => {
-  return (
-    <section>
-      <h1>{text}</h1>
-    </section>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <section>
-      {parts.map((item) => (
-        <Part part={item.name} exercises={item.exercises} key={item.id} />
-      ))}
-    
-    </section>
-  );
-};
-
-const Part = ({ part, exercises }) => {
-  return (
-    <section>
-      <p>
-        {part} {exercises}
-      </p>
-    </section>
-  );
-};
-
-const Stats = ({ sum }) => {
-   const total = sum.reduce((a, c) => a + c.exercises, 0);
-
-  return (
-    <section>
-      <h4>Total of {total} exercises</h4>
     </section>
   );
 };
