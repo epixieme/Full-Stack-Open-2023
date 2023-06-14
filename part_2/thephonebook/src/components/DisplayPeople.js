b1f16f535cc7c07f97116bc6ad71bd43ff6128b7
epixieme <kirstie_hayes@hotmail.com>
2023-06-14 07:46:57 +0100

    exercise 2.9-2.10

--- part_2/thephonebook/src/components/DisplayPeople.js
+++ part_2/thephonebook/src/components/DisplayPeople.js
@@ -1,19 +1,13 @@
 const DisplayPeople = ({ persons, search }) => {
-  return (
-    <section>
-      {persons.map((person) => {
-        if (person.name.toLowerCase().includes(search.toLowerCase())) {
-          return (
-            <p>
-            {person.name} {person.number}
-          </p>
-          )
-        }
-      })}
-    </section>
+  const people = persons.map((person, ind) =>
+  // still returns names as default because the search state is an empty string to start with so empty string returns true
+    person.name.toLowerCase().includes(search.toLowerCase()) ? (
+      <p key={ind + 1}>
+        {person.name} {person.number}
+      </p>
+    ) : null 
   );
+  return <section>{people}</section>;
 };
 
 export default DisplayPeople;
-
-
