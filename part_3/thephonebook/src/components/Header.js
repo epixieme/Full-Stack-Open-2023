320115b7716590a5cb290bfd7ba6f21e58e88c0b
epixieme <kirstie_hayes@hotmail.com>
2023-06-27 16:04:38 +0100

    removing part 3

--- part_3/thephonebook/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = ({ text }) => {
-  return (
-    <section>
-      <h1>{text}</h1>
-    </section>
-  );
-};
-
-export default Header;
--- part_3/thephonebook/src/components/SearchPerson.js
+++
@@ -1,9 +0,0 @@
-const SearchPerson = ({ onChange, search }) => {
-  return (
-    <section>
-      <p>Filter Shown with</p> <input value={search} onChange={onChange} />
-    </section>
-  );
-};
-
-export default SearchPerson;
